ff6e42f059263c29e1642194af73384a1ba5a6df
unknown <pcurry@phillip-curry.jonahgroup.com>
2013-12-04 16:14:03 -0500

    switched dao to pg

--- tastyDao.js
+++ tastyDao.js
@@ -1,15 +1,12 @@
 	//Imports
 	var fs = require('fs');
-	var sqlite3 = require('sqlite3').verbose();
+	var adapter = require('pg');
 	
 	//Fields
 	var file = "tasting.db";
 	var db = new sqlite3.Database(file);
 	
 	//Init
-	if (!fs.existsSync(file)){
-		runSql("CREATE TABLE TASTES (TYPE VARCHAR2, NAME VARCHAR2, RATING INT)");
-	}
 	
 	reportError = function (err){
 		console.log(err);
@@ -22,11 +19,17 @@
 	exports.submitEntry = function(params){
 		var sql = 'INSERT INTO TASTES (TYPE, NAME, RATING) VALUES (?,?,?)';
 		console.log(sql);
-		db.run(sql, params.type, params.name, params.rating, reportError, reportSuccess);
+		adapter.connect(process.env.DATABASE_URL, function(err, client){
+			var query = client.query(sql, [params.type, params.name, params.rating]);
+			client.end();
+		});
 	};
 	
 	exports.getEntries = function(callback){
 		var sql = 'SELECT ROWID, * FROM TASTES';
 		console.log(sql);
-		db.all(sql, callback);
+		adapter.connect(process.env.DATABASE_URL, function(err, client){
+			client.query(sql, callback);
+			client.end();
+			});
 	};
