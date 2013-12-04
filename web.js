fb62560fcc0cb98e508750b5ebd972a9ae8c5c16
unknown <pcurry@phillip-curry.jonahgroup.com>
2013-12-04 12:57:28 -0500

    added logging

--- web.js
+++ web.js
@@ -1,10 +1,11 @@
-
-var __dirname = './';
-
 var http = require('http');
 
+console.log("creating Server");
 var server = http.createServer(function (req, res){
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end("Hello World\n");
+	console.log("server created");
+
 });
-server.listen(8000);
+console.log("listening to port 8080");
+server.listen(8080);
