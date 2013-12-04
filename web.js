098f0641fb84fc956e7552b9a6ba101d22de35b3
unknown <pcurry@phillip-curry.jonahgroup.com>
2013-12-04 12:53:15 -0500

    fixed send to end

--- web.js
+++ web.js
@@ -5,6 +5,6 @@ var http = require('http');
 
 var server = http.createServer(function (req, res){
 	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.send("Hello World\n");
+	res.end("Hello World\n");
 });
 server.listen(8000);
