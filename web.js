767c6c21eba58236d7093761c27c8771884d79e8
unknown <pcurry@phillip-curry.jonahgroup.com>
2013-12-04 10:08:30 -0500

    adding web.js for heroku

---
+++ web.js
@@ -0,0 +1,39 @@
+var express= require('express');
+var fs = require('fs');
+var doT = require('express-dot');
+
+
+var tastyDao = require('./tastyDao.js');
+
+var __dirname = './';
+var app = express();
+
+//Set up Express to serve doT views
+app.configure(function(){
+	app.use(express.bodyParser());
+	app.set('views', __dirname+'/view/');
+	app.set('view engine', 'doT');
+	app.engine('html', doT.__express);
+});
+
+//Serve public folder for static content
+app.use('/public', express.static(__dirname+'/public'));
+
+//get handlers
+app.get('/', function(req, res){
+	var entries = tastyDao.getEntries(
+			function(err, entries){
+				res.render('index.html', {entries: entries});
+			});
+});
+app.get('/addEntry.html', function(req, res){
+	res.render('addEntry.html');
+});
+
+app.post('/submitEntry.html', function(req, res){
+	console.log(req.body);
+	tastyDao.submitEntry(req.body);
+	res.redirect('/');
+});
+
+app.listen(8080);
